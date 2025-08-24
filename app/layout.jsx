028f0f146114b92246e0b3898e1a2dff3bc5a33b
Mujeebullah Kalhoro <mujeebullahkalhoro77@gmail.com>
2025-08-24 23:43:32 +0500

    feat: display products on homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-
+import { AppContextProvider } from "@/context/AppContext";
 const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
 
 export const metadata = {
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${outfit.className} antialiased text-gray-700 min-h-screen`}>
+        <AppContextProvider>
         <NavBar/>
         {children}
         <Footer />
+        </AppContextProvider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeaderSlider from "@/components/HeaderSlider";
+import HomeProducts from "@/components/HomeProducts";
 export default function Home() {
   return (
     
     <div className="px-6 md:px-16 lg:px-32">
       <HeaderSlider />
+      <HomeProducts/>
     </div>
   );
 }
